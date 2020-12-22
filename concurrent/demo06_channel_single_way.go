240bbcba4e472b2ed6c1f41cc9b0604e5f99a870
Redbo He <redbo.he@gshopper.com>
2020-12-22 20:16:08 +0800

    增加 通道错误与 select 示例代码

---
+++ concurrent/demo06_channel_single_way.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func test2(ch chan<- int) {
+	for i := 0; i < 100; i++ {
+		ch <- 1
+	}
+	close(ch)
+}
+
+func main() {
+	start := time.Now()
+	ch := make(chan int, 20)
+	//ch := make(chan int)
+	go test2(ch)
+	for i := range ch {
+		fmt.Println("接收到的数据:", i)
+	}
+	end := time.Now()
+	consume := end.Sub(start).Seconds()
+	fmt.Println("程序执行耗时(s)：", consume)
+}
