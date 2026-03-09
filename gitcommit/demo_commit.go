d32a7f708f503624480c54c46a23f585f6bce6c6
hehongbo <hhbjkd@163.com>
2026-03-09 09:59:43 +0800

    第一次提交代码 补充代码

--- gitcommit/demo_commit.go
+++ gitcommit/demo_commit.go
@@ -1,6 +1,10 @@
 package gitcommit
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gookit/goutil/dump"
+)
 
 func main() {
 
@@ -8,10 +12,12 @@ func main() {
 
 func DemoAction1() {
 	fmt.Println("Demo Action 1")
+	dump.P("this is add  DemoAction1 Content")
 }
 
 func DemoAction2() {
 	fmt.Println("Demo Action 2")
+	dump.P("this is add  DemoAction2 Content")
 }
 
 func DemoAction3() {
